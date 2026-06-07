19364f268a274326d8826593511af99db30e69f1
aether0xff-afk <aether0xff@gmail.com>
2026-06-07 13:12:51 +0900

    update package exports

--- v7/src/emonet_v7/__init__.py
+++ v7/src/emonet_v7/__init__.py
@@ -1,12 +1,18 @@
 """EmoNet v7 adaptive sparse RSNN core."""
 
 from .adaptive_rsnn import AdaptiveSparseRSNN, SNNState, TickTrace, create_recurrent_mask
+from .event_encoder import EventEncoder
+from .schemas import Event
 from .surrogate import spike_with_surrogate_gradient
+from .trace_encoder import TraceEncoder
 
 __all__ = [
     "AdaptiveSparseRSNN",
     "SNNState",
     "TickTrace",
     "create_recurrent_mask",
+    "EventEncoder",
+    "Event",
+    "TraceEncoder",
     "spike_with_surrogate_gradient",
 ]
